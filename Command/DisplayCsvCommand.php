735e82bceb6e3e7184c02818d07c63ff263366e0
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-03-31 17:23:16 +0200

    :construction: Csv render as a Table

--- Command/DisplayCsvCommand.php
+++ Command/DisplayCsvCommand.php
@@ -3,12 +3,13 @@ Namespace CsvDisplayTool\Command;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Helper\Table;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class DisplayCsvCommand extends Command
 {
-    protected static $defaultName = 'tool:display-csv';
+    protected static $defaultName = 'csv:display-csv';
 
     protected function configure(): void
     {
@@ -18,8 +19,11 @@ class DisplayCsvCommand extends Command
 
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $text = 'Hi' .$input->getArgument('filename');
-        $output->writeln($text.'!');
+        $table = new Table($output);
+        $table
+            ->setHeaders(['Sku', 'Status', 'Price', 'Description', 'CreatedAt', 'slug'])
+        ;
+        $table->render();
         return 0;
     }
 }
---
+++ CsvParser/CsvParser.php
@@ -0,0 +1,7 @@
+<?php
+Namespace CsvDisplayTool\CsvParser\CsvParser;
+
+class CsvParser
+{
+
+}

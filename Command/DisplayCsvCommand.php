a335963b4e7e8fb9c6ef676524dce79b7e816ab5
Wilhem Lecanu <wilhem.lecanu@edu.devinci.fr>
2022-04-01 11:30:56 +0200

    :construction: wip Table Format & OOP model

--- Command/DisplayCsvCommand.php
+++ Command/DisplayCsvCommand.php
@@ -6,7 +6,10 @@ use CsvDisplayTool\CsvParser\CsvParser;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\Encoder\JsonEncoder;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 ini_set('auto_detect_line_endings',TRUE);
@@ -19,6 +22,7 @@ class DisplayCsvCommand extends Command
     {
         $this->setDescription('Allows you to Display Csv File in CLI');
         $this->addArgument('filename', InputArgument::OPTIONAL, 'Target File');
+        $this->addOption('json', 'j', InputOption::VALUE_NONE);
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): int
@@ -29,13 +33,21 @@ class DisplayCsvCommand extends Command
         $parsedArray = $parseImport->csvParser($input->getArgument('filename'));
         $formattedArray = $formatterImport->formatArray($parsedArray);
 
-        var_dump($formattedArray);
+        if($input->getOption('json') === true)
+        {
+            $encoders = [ new JsonEncoder()];
+            $serializer = new Serializer([], $encoders);
 
-        $table = new Table($output);
-        $table
-            ->setHeaders([]);
-        ;
-        $table->render();
+            $output->writeln($serializer->serialize($formattedArray, 'json', ['json_encode_options' => \JSON_PRESERVE_ZERO_FRACTION]));
+        } else {
+            var_dump($formattedArray);
+            $table = new Table($output);
+            $table
+                ->setHeaders($formattedArray[0])
+                ->setRow(1, $formattedArray[1])
+                ->setRow(2, $formattedArray[2]);
+            $table->render();
+        }
         return 0;
     }
 }
--- CsvFormatter/csvFormatter.php
+++ CsvFormatter/csvFormatter.php
@@ -1,9 +1,69 @@
 <?php
 Namespace CsvDisplayTool\CsvFormatter;
 
-class csvFormatter{
-    public function formatArray($toFormatArray)
+use Cassandra\Date;
+use DateTime;
+
+class csvFormatter {
+
+    public function slugFormat(string $slug):string
     {
+
+        $output = str_replace(array(',', ';', '<', '>', '/'),'' , strtolower($slug));
+        return str_replace(' ', '-', $output);
+    }
+
+    public function PriceFormat(string $value): string
+    {
+       $input = (float) $value;
+       $output = number_format($input, 2, ',', ' ');
+       return $output. " €";
+    }
+    public function dateFormat($dateTime)
+    {
+        $date = DateTime::createFromFormat('Y-m-d H:i:s', $dateTime);
+        return date_format($date, 'l, d-M-Y H:i:s e');
+    }
+
+    public function formatStatus($isEnabled):string
+    {
+        if($isEnabled)
+        {
+            return "Enable";
+        } else {
+            return "disable";
+        }
+    }
+
+    public function descriptionFormat($toFormat): string
+    {
+        return nl2br($toFormat, false);
+    }
+
+    public function formatArray($toFormatArray): array
+    {
+        unset($toFormatArray[0][4]);
+        unset($toFormatArray[1][4]);
+        unset($toFormatArray[2][4]);
+        $toFormatArray = array_values($toFormatArray);
+
+        $toFormatArray[0][] = 'slug';
+        $toFormatArray[1][] = $this->slugFormat($toFormatArray[1][1]);
+        $toFormatArray[2][] = $this->slugFormat($toFormatArray[2][1]);
+
+        $toFormatArray[1][3] = $this->PriceFormat($toFormatArray[1][3]);
+        $toFormatArray[2][3] = $this->PriceFormat($toFormatArray[2][3]);
+
+        $toFormatArray[1][6] = $this->dateFormat($toFormatArray[1][6]);
+        $toFormatArray[2][6] = $this->dateFormat($toFormatArray[2][6]);
+
+        $toFormatArray[1][2] = $this->formatStatus($toFormatArray[1][2]);
+        $toFormatArray[2][2] = $this->formatStatus($toFormatArray[2][2]);
+
+        $toFormatArray[1][5] = $this->descriptionFormat($toFormatArray[1][5]);
+        $toFormatArray[2][5] = $this->descriptionFormat($toFormatArray[2][5]);
+
+
 
         return $toFormatArray;
     }
